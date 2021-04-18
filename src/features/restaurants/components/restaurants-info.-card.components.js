6678bbbc20d91ea5dc2f1814a9f579df93c234b6
Devavrat Singh <43301449+satsaiinfocom@users.noreply.github.com>
2021-04-18 20:00:34 +0530

    Layout improoved.

--- src/features/restaurants/components/restaurants-info.-card.components.js
+++ src/features/restaurants/components/restaurants-info.-card.components.js
@@ -1,33 +1,88 @@
 import React from "react";
 import styled from "styled-components/native";
-import { Text, StyleSheet } from "react-native";
+import { Text, Image, View } from "react-native";
 import { Card } from "react-native-paper";
+import { SvgXml } from "react-native-svg";
+import star from "../../../../assets/star";
+import open from "../../../../assets/open";
+const RestaurantCard = styled(Card)`
+  background-color: ${(props) => props.theme.colors.bg.primary};
+`;
+const RestaurantCardCover = styled(Card.Cover)`
+  padding: ${(props) => props.theme.space[3]};
+  background-color: ${(props) => props.theme.colors.bg.primary};
+`;
+const Address = styled.Text`
+  font-size: ${(props) => props.theme.fontSizes.caption};
+  font-family: ${(props) => props.theme.fonts.heading};
+  color: ${(props) => props.theme.colors.ui.primary};
+`;
 const Title = styled.Text`
-  padding: 16px;
-  color: red;
+  font-size: ${(props) => props.theme.fontSizes.body};
+  font-family: ${(props) => props.theme.fonts.body}
+  color: ${(props) => props.theme.colors.ui.primary};
+`;
+const Info = styled.View`
+  padding: ${(props) => props.theme.space[0]};
+`;
+const Rating = styled.View`
+  flex-direction: row;
+  padding-top: ${(props) => props.theme.space[2]};
+  padding-bottom: ${(props) => props.theme.space[2]};
+`;
+const Section = styled.View`
+  flex-direction: row;
+  align-items: center;
+`;
+const SectionEnd = styled.View`
+  flex: 1;
+  flex-direction: row;
+  justify-content: flex-end;
+`;
+const Open = styled.View`
+  flex-direction: row;
 `;
 export const RestaurantInfoCard = ({ restaurant = {} }) => {
   const {
     name = "Some Restaurant",
-    icon,
+    icon = "https://maps.gstatic.com/mapfiles/place_api/icons/v1/png_71/lodging-71.png",
     photos = [
       "https://www.foodiesfeed.com/wp-content/uploads/2019/06/top-view-for-box-of-2-burgers-home-made-600x899.jpg",
     ],
     address = "100 some random street",
     isOpenNow = true,
     rating = 4,
-    isClosedTemporarily,
+    isClosedTemporarily = true,
   } = restaurant;
+  const ratingArray = Array.from(new Array(Math.floor(rating)));
   return (
-    <Card elevation={5} style={styles.card}>
-      <Card.Cover key={name} style={styles.cover} source={{ uri: photos[0] }} />
+    <RestaurantCard elevation={5}>
+      <RestaurantCardCover key={name} source={{ uri: photos[0] }} />
       <Card.Content>
-        <Title>{name}</Title>
+        <Info>
+          <Title>{name}</Title>
+          <Section>
+            <Rating>
+              {ratingArray.map(() => (
+                <SvgXml xml={star} width={20} height={20} />
+              ))}
+            </Rating>
+            <SectionEnd>
+              {isClosedTemporarily && (
+                <Text variant="label" style={{ color: "red" }}>
+                  CLOSED TEMPORARILY
+                </Text>
+              )}
+              <View style={{ paddingLeft: 16 }} />
+              {isOpenNow && <SvgXml xml={open} width={20} height={20} />}
+              <View style={{ paddingLeft: 16 }} />
+              <Image style={{ width: 15, height: 15 }} source={{ uri: icon }} />
+            </SectionEnd>
+          </Section>
+
+          <Address>{address}</Address>
+        </Info>
       </Card.Content>
-    </Card>
+    </RestaurantCard>
   );
 };
-const styles = StyleSheet.create({
-  card: { backgroundColor: "white" },
-  cover: { padding: 20, backgroundColor: "white" },
-});
