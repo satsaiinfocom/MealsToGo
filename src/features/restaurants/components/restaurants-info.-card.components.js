395514d8c0edb680c46c5375fa1b4e2bc2d81fba
Devavrat Singh <43301449+satsaiinfocom@users.noreply.github.com>
2021-04-26 15:25:52 +0530

    New Updates for the navigation tabs added into the application.

--- src/features/restaurants/components/restaurants-info.-card.components.js
+++ src/features/restaurants/components/restaurants-info.-card.components.js
@@ -1,88 +1,58 @@
 import React from "react";
-import styled from "styled-components/native";
-import { Text, Image, View } from "react-native";
-import { Card } from "react-native-paper";
 import { SvgXml } from "react-native-svg";
+import { Spacer } from "../../../components/spacer/spacer.component";
+import { Text } from "../../../components/typography/text.component";
 import star from "../../../../assets/star";
 import open from "../../../../assets/open";
-const RestaurantCard = styled(Card)`
-  background-color: ${(props) => props.theme.colors.bg.primary};
-`;
-const RestaurantCardCover = styled(Card.Cover)`
-  padding: ${(props) => props.theme.space[3]};
-  background-color: ${(props) => props.theme.colors.bg.primary};
-`;
-const Address = styled.Text`
-  font-size: ${(props) => props.theme.fontSizes.caption};
-  font-family: ${(props) => props.theme.fonts.heading};
-  color: ${(props) => props.theme.colors.ui.primary};
-`;
-const Title = styled.Text`
-  font-size: ${(props) => props.theme.fontSizes.body};
-  font-family: ${(props) => props.theme.fonts.body}
-  color: ${(props) => props.theme.colors.ui.primary};
-`;
-const Info = styled.View`
-  padding: ${(props) => props.theme.space[0]};
-`;
-const Rating = styled.View`
-  flex-direction: row;
-  padding-top: ${(props) => props.theme.space[2]};
-  padding-bottom: ${(props) => props.theme.space[2]};
-`;
-const Section = styled.View`
-  flex-direction: row;
-  align-items: center;
-`;
-const SectionEnd = styled.View`
-  flex: 1;
-  flex-direction: row;
-  justify-content: flex-end;
-`;
-const Open = styled.View`
-  flex-direction: row;
-`;
+import {
+  Icon,
+  RestaurantCard,
+  RestaurantCardCover,
+  Address,
+  Info,
+  Rating,
+  Section,
+  SectionEnd,
+  Open,
+} from "./restaurant-info-card.styles";
+
 export const RestaurantInfoCard = ({ restaurant = {} }) => {
   const {
     name = "Some Restaurant",
     icon = "https://maps.gstatic.com/mapfiles/place_api/icons/v1/png_71/lodging-71.png",
     photos = [
-      "https://www.foodiesfeed.com/wp-content/uploads/2019/06/top-view-for-box-of-2-burgers-home-made-600x899.jpg",
+      "https://images.pexels.com/photos/1640777/pexels-photo-1640777.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
     ],
     address = "100 some random street",
     isOpenNow = true,
-    rating = 4,
+    rating = 5,
     isClosedTemporarily = true,
   } = restaurant;
   const ratingArray = Array.from(new Array(Math.floor(rating)));
   return (
     <RestaurantCard elevation={5}>
       <RestaurantCardCover key={name} source={{ uri: photos[0] }} />
-      <Card.Content>
-        <Info>
-          <Title>{name}</Title>
-          <Section>
-            <Rating>
-              {ratingArray.map(() => (
-                <SvgXml xml={star} width={20} height={20} />
-              ))}
-            </Rating>
-            <SectionEnd>
-              {isClosedTemporarily && (
-                <Text variant="label" style={{ color: "red" }}>
-                  CLOSED TEMPORARILY
-                </Text>
-              )}
-              <View style={{ paddingLeft: 16 }} />
-              {isOpenNow && <SvgXml xml={open} width={20} height={20} />}
-              <View style={{ paddingLeft: 16 }} />
-              <Image style={{ width: 15, height: 15 }} source={{ uri: icon }} />
-            </SectionEnd>
-          </Section>
+      <Info>
+        <Text variant="label">{name}</Text>
+        <Section>
+          <Rating>
+            {ratingArray.map(() => (
+              <SvgXml xml={star} width={20} height={20} />
+            ))}
+          </Rating>
+          <SectionEnd>
+            {isClosedTemporarily && (
+              <Text variant="error">CLOSED TEMPORARILY</Text>
+            )}
+            <Spacer position="left" size="large" />
+            {isOpenNow && <SvgXml xml={open} width={20} height={20} />}
+            <Spacer position="left" size="large" />
+            <Icon source={{ uri: icon }} />
+          </SectionEnd>
+        </Section>
 
-          <Address>{address}</Address>
-        </Info>
-      </Card.Content>
+        <Address>{address}</Address>
+      </Info>
     </RestaurantCard>
   );
 };
